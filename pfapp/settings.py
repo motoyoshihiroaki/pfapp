abbceb1b9bde0c7838ad768c28a44b27f9cf680d
Hiroaki_M <motoyoshihiroaki@gmail.com>
2020-12-31 09:31:58 +0000

    静的ファイルのパスを記述

--- pfapp/settings.py
+++ pfapp/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/share/nginx/html/static'
 
 # 静的ファイルの追加
 STATICFILES_DIRS = (
